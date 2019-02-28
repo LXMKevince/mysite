de8c5e8a62b6ba7b81c5bb6fc6ba220f6cce33dc
LXMKevince <215120986@qq.com>
2019-02-28 22:31:55 +0800

    新增富文本编编辑器

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from DjangoUeditor.models import UEditorField  # 头部增加这行代码导入UEditorField
 
 
 class BlogType(models.Model):
@@ -11,8 +12,15 @@ class BlogType(models.Model):
 
 class Blog(models.Model):
     title = models.CharField(max_length=50)
-    blog_type = models.ForeignKey(BlogType, on_delete=models.DO_NOTHING)
-    content = models.TextField()
+    blog_type = models.ForeignKey(
+        BlogType, on_delete=models.DO_NOTHING)
+    # content = models.TextField()
+    body = UEditorField('content', width=800, height=500,
+                        toolbars="full", imagePath="upimg/", filePath="upfile/",
+                        upload_settings={"imageMaxSize": 1204000},
+                        settings={}, command=None, blank=True
+                        )
+
     author = models.ForeignKey(User, on_delete=models.DO_NOTHING)
     created_time = models.DateTimeField(auto_now_add=True)
     last_updated_time = models.DateTimeField(auto_now=True)
--- mysite/urls.py
+++ mysite/urls.py
@@ -14,11 +14,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+# 上面这行多加了一个re_path
+from django.views.static import serve
+# 导入静态文件模块
+from django.conf import settings
+# 导入配置文件里的文件上传配置
 from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls')),
+    path('ueditor/', include('DjangoUeditor.urls')),  # 添加DjangoUeditor的URL
+    re_path('^media/(?P<path>.*)$', serve,
+            {'document_root': settings.MEDIA_ROOT}),  # 增加此行
 ]
