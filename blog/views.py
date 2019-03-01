40c07c3bc9c9aaa5eaeeb9db1e268026da385335
LXMKevince <215120986@qq.com>
2019-03-01 22:28:30 +0800

    前端页面书写

--- blog/views.py
+++ blog/views.py
@@ -3,8 +3,10 @@ from .models import Blog, BlogType
 
 
 def blog_list(request):
-    blogs = Blog.objects.all()
-    return render(request, 'blog/blog_list.html', {'blogs': blogs})
+    context = {}
+    context['blogs'] = Blog.objects.all()
+    context['blog_types'] = BlogType.objects.all()
+    return render(request, 'blog/blog_list.html', context)
 
 
 def blog_detail(request, blog_pk):
@@ -17,4 +19,5 @@ def blogs_with_type(request, blog_type_pk):
     blog_type = get_object_or_404(BlogType, pk=blog_type_pk)
     context['blogs'] = Blog.objects.filter(blog_type=blog_type)
     context['blog_type'] = blog_type
+    context['blog_types'] = BlogType.objects.all()
     return render(request, 'blog/blogs_with_type.html', context)
